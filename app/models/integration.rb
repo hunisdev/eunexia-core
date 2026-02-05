7b35e43448d3b91361972f5221d65a57b653d086
Hunis Dev <hunisdev@gmail.com>
2026-02-06 05:29:16 +0800

    Add validation to Integration name, source_system; Add associations to inbound_runs, raw_events and orders models

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,2 +1,7 @@
 class Integration < ApplicationRecord
+  has_many :inbound_runs, dependent: :destroy
+  has_many :raw_events, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :name, :source_system, presence: true
 end
